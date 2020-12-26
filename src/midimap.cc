4a1ac7da02c24f8a199b34c70b8f3f6357a4cf0d
Jason Kimko <22348009+jskimko@users.noreply.github.com>
2020-12-25 21:50:48 -0500

    Remove unnecessary fltk headers.

--- src/midimap.cc
+++ src/midimap.cc
@@ -2,13 +2,8 @@
 #include "midimap/SetupWindow.hh"
 
 #include "FL/Fl.H"
-#include "FL/Fl_Window.H"
-#include <FL/Fl_Box.H>
 #include <FL/Fl_Choice.H>
 #include <FL/Fl_Button.H>
-#include <FL/Fl_Light_Button.H>
-#include <FL/Fl_Input.H>
-#include <FL/Fl_Multiline_Output.H>
 
 #include <cstdio>
 
